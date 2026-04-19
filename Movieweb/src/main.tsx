29a2e552ea10707cb33f1b31d3a22efa041b0d6f
ÁngelDomínguezstz <luis.angel.sd.12@gmail.com>
2026-04-18 23:56:09 -0600

    corrigio un directorio

--- Movieweb/src/main.tsx
+++ Movieweb/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 
-import MoviePages from './Pages/MoviePages.tsx';
+import MoviePages from './pages/MoviePages.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
